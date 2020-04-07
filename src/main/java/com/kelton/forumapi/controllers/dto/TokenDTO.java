3899476f73d70a03c4b9e93c63c40ca353204d90
kelton99 <keltonfreitas53@gmail.com>
2020-04-06 22:36:17 -0300

    JWT generation added

---
+++ src/main/java/com/kelton/forumapi/controllers/dto/TokenDTO.java
@@ -0,0 +1,23 @@
+package com.kelton.forumapi.controllers.dto;
+
+public class TokenDTO {
+	
+	private String token;
+	private String tipo;
+
+	public TokenDTO(String token, String tipo) {
+		this.token = token;
+		this.tipo = tipo;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+	
+	
+
+}
